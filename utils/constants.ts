968c7c5f26cbd6ef12241452a6754e8a3f14ccad
noman <itservicesusa71@gmail.com>
2021-05-13 22:41:59 -0700

    update pagination

--- utils/constants.ts
+++ utils/constants.ts
@@ -7,9 +7,9 @@ export const API_URL =
 export const MAX_ETH = utils.parseEther("0.014");
 
 // Always keep these below 50 to ensure OpenSea Api can handle the requests
-export const TOKENS_IN_HOME = 40;
-export const SOLD_TOKENS_IN_HOME = 40;
-export const UPCOMING_TOKENS_IN_HOME = 40;
+export const TOKENS_IN_HOME = 28;
+export const SOLD_TOKENS_IN_HOME = 8;
+export const UPCOMING_TOKENS_IN_HOME = 8;
 
 export const TOKENS_PER_PAGE = 40;
 export const ARTISTS_PER_PAGE = 40;
